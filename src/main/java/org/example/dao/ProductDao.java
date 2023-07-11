3aa91769ddf87dda912f29a731657dbdf3c0b358
Erick Michel <michel172002@gmail.com>
2023-07-10 22:31:21 -0300

    created ProductDao and method to register Product

---
+++ src/main/java/org/example/dao/ProductDao.java
@@ -0,0 +1,17 @@
+package org.example.dao;
+
+import org.example.model.Product;
+
+import javax.persistence.EntityManager;
+
+public class ProductDao {
+    private EntityManager em;
+
+    public ProductDao(EntityManager em) {
+        this.em = em;
+    }
+
+    public void register(Product product){
+        this.em.persist(product);
+    }
+}
